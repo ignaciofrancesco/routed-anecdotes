373de6520fd8c14796d04861529796305f6ad4f5
ignacio <ignaciofranciscovigo@gmail.com>
2025-01-13 14:27:45 -0300

    Ex 7.6

--- src/App.jsx
+++ src/App.jsx
@@ -92,9 +92,9 @@ const CreateNew = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: content.attributes.value,
+      author: author.attributes.value,
+      info: info.attributes.value,
       votes: 0,
     });
 
@@ -114,15 +114,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...content.attributes} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...author.attributes} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...info.attributes} />
         </div>
         <button type="submit">create</button>
         <button type="button" onClick={handleClickReset}>
